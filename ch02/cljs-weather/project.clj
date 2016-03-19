7a64d579c8e760d0a2e584b73ca74f59be1b8688
Andrew Meredith <andrew.meredith@intrinsitech.net>
2016-03-19 10:20:47 -0600

    Finish sample app

--- ch02/cljs-weather/project.clj
+++ ch02/cljs-weather/project.clj
@@ -10,7 +10,8 @@
                  [org.clojure/clojurescript "1.7.170"]
                  [org.clojure/core.async "0.2.374"
                   :exclusions [org.clojure/tools.reader]]
-                 [reagent "0.5.1"]]
+                 [reagent "0.5.1"]
+                 [cljs-ajax "0.5.3"]]
   
   :plugins [[lein-figwheel "0.5.0-6"]
             [lein-cljsbuild "1.1.2" :exclusions [[org.clojure/clojure]]]]
--- ch02/cljs-weather/src/cljs_weather/core.cljs
+++ ch02/cljs-weather/src/cljs_weather/core.cljs
@@ -1,23 +1,57 @@
 (ns cljs-weather.core
-  (:require [reagent.core :as reagent :refer [atom]]))
+  (:require [reagent.core :as reagent :refer [atom]]
+            [ajax.core :refer [GET]]))
 
 (enable-console-print!)
 
-(println "Edits to this text should show up in your developer console.")
+(defonce app-state (atom {:title "WhichWeather"
+                          :postal-code ""
+                          :data-received? false
+                          :temperatures {:today {:label "Today"
+                                                 :value nil}
+                                         :tomorrow {:label "Tomorrow"
+                                                    :value nil}}}))
+(def api-key "848a2b6baab0b48df060709c4142c09e")
 
-;; define your app data so that it doesn't get over-written on reload
+(defn handle-response [resp]
+  (let [today (get-in resp ["list" 0 "main" "temp"])
+        tomorrow (get-in resp ["list" 23 "main" "temp"])]
+    (swap! app-state update-in [:temperatures :today :value] (constantly today))
+    (swap! app-state update-in [:temperatures :tomorrow :value] (constantly tomorrow))))
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defn get-forecast! []
+  (let [postal-code (:postal-code @app-state)]
+    (GET "http://api.openweathermap.org/data/2.5/forecast"
+         {:params {"q" postal-code
+                   "units" "imperial"
+                   "appid" api-key}
+          :handler handle-response})))
 
-(defn hello-world []
-  [:h1 (:text @app-state)])
+(defn title []
+  [:h1 (:title @app-state)])
 
-(reagent/render-component [hello-world]
+(defn temperature [temp]
+  [:div {:class "temperature"}
+   [:div {:class "value"}
+    (:value temp)]
+   [:h2 (:label temp)]])
+
+(defn postal-code []
+  [:div {:class-name "postal-code"}
+   [:h3 "Enter your postal code"]
+   [:input {:type "text"
+            :placeholder "Postal Code"
+            :value (:postal-code @app-state)
+            :on-change #(swap! app-state assoc :postal-code (-> % .-target .-value))}]
+   [:button {:on-click get-forecast!} "Go"]])
+
+(defn app []
+  [:div {:class "app"}
+   [title]
+   [:div {:class "temperatures"}
+    (for [temp (vals (:temperatures @app-state))]
+      [temperature temp])]
+   [postal-code]])
+
+(reagent/render-component [app]
                           (. js/document (getElementById "app")))
-
-
-(defn on-js-reload []
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
